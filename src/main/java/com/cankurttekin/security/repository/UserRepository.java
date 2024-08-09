6cd5fcf725477fd2447bd85895d39d29ba30718f
can kurttekin <cankurttekin@gmail.com>
2024-08-09 09:39:09 +0300

    userrepository

---
+++ src/main/java/com/cankurttekin/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cankurttekin.security.repository;
+
+import com.cankurttekin.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
